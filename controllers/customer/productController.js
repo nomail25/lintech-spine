e875149816b8b63d0761a2b15ff2f8b7c7f78244
titas2003 <titas20031996@gmail.com>
2025-02-12 20:21:54 +0530

    created product searches

--- controllers/customer/productController.js
+++ controllers/customer/productController.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const Product = require('../../models/seller/Product');
+const Category = require('../../models/seller/Category');
 
 exports.getAllProducts = async (req, res) => {
     try {
         const products = await Product.find();
         res.status(200).json(products);
-    } catch(error) {
+    } catch (error) {
         res.status(500).json({ message: 'Server Error' });
     }
 }
+
+exports.getProductByCategory = async (req, res) => {
+    const { name } = req.params;
+
+    try {
+        const category = await Category.findOne({ name: name });
+        console.log(category);
+
+        if (!category) {
+            return res.status(404).json({ message: 'Category not found' });
+        }
+
+        const products = await Product.find({ category: category._id });
+
+        if (products.length === 0) {
+            return res.status(404).json({ message: 'No products found in this category' });
+        }
+
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+}
+
+exports.getProductByVendor = async (req, res) => {
+    const { vendor } = req.params;
+    const vendorName = vendor.toLowerCase();
+    try {
+        const products = await Product.find({ vendor: vendorName });
+        if (products.length === 0) {
+            return res.status(404).json({ message: 'No products found in this category' });
+        }
+        console.log("returned search on "+vendor+" : \n"+products)
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
--- routes/customer/productRoutes.js
+++ routes/customer/productRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getAllProducts } = require('../../controllers/customer/productController');
+const { getAllProducts, getProductByCategory, getProductByVendor } = require('../../controllers/customer/productController');
 const router = express.Router();
 
 router.get("/getAllProducts", getAllProducts);
+router.get("/getProdByCategory/:name", getProductByCategory);
+router.get("/getProductByVendor/:vendor", getProductByVendor)
 
 module.exports = router;
