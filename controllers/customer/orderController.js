5d2b4cbea011e5f70c6df4bf75ddaa57246f91e4
titas2003 <titas20031996@gmail.com>
2025-02-13 13:15:19 +0530

    added orderOne api

--- controllers/customer/orderController.js
+++ controllers/customer/orderController.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 const Cart = require('../../models/customer/Cart')
+const Order = require('../../models/customer/Order');
 
 exports.createCart = async (req, res) => {
     try {
@@ -25,5 +24,36 @@ exports.createCart = async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ success: false, message: "Server error", error: error.message });
+    }
+}
+
+exports.createOrderOne = async (req, res) => {
+    const { productId, quantity, price } = req.body;
+    const customerId = req.user.id;  // Retrieved from JWT token
+    const address = req.user.address; // Address stored in JWT token
+    console.log(req.user)
+
+    // Generate a random order ID for simplicity (you could use something more sophisticated)
+    const orderId = `LINTECH-${Date.now()}`;
+    const orderDate = new Date();
+    const estimatedDeliveryDate = new Date(orderDate.setDate(orderDate.getDate() + 5));
+
+
+    try {
+        const newOrder = new Order({
+            customerId,
+            productId,
+            quantity,
+            price,
+            address,
+            estimatedDeliveryDate,
+            orderId,
+        });
+
+        await newOrder.save();
+        res.status(201).json(newOrder);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error, message: "Error creating order", error });
     }
 }
